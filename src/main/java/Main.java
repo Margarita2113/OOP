338e2796f8f921497f4b8e3c22e2500cbefc2212
Oskina Margarita <rita_2113@mail.ru>
2023-05-31 12:54:27 +0300

    Changed subscription

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
         post.surname = "Иванов";
         post.passport = "4444 № 44444444";
         post.phone = "+7 (999)-999-99-99";
-        post.subscription = "true";
+        post.subscription = "boolean";
 
         post.birthday.day = 13;
         post.birthday.month = 6;
